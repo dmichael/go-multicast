097bfd85720a48ac49f28832dc3cb92fdaff368f
David Michael <dmichael@users.noreply.github.com>
2019-10-21 11:23:23 -0400

    #1 temp resolution to ipv4 on deb 9+ (#2)

--- multicast/broadcaster.go
+++ multicast/broadcaster.go
@@ -4,12 +4,12 @@ import "net"
 
 // NewBroadcaster creates a new UDP multicast connection on which to broadcast
 func NewBroadcaster(address string) (*net.UDPConn, error) {
-	addr, err := net.ResolveUDPAddr("udp", address)
+	addr, err := net.ResolveUDPAddr("udp4", address)
 	if err != nil {
 		return nil, err
 	}
 
-	conn, err := net.DialUDP("udp", nil, addr)
+	conn, err := net.DialUDP("udp4", nil, addr)
 	if err != nil {
 		return nil, err
 	}
